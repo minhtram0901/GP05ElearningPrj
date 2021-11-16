bc3cab3477262ee6211480a5edd67589606b8b78
minh tram <minhtram0901@gmail.com>
2021-11-16 23:15:25 +0700

    done layout admin-auth

--- src/app/pages/admin-auth/admin-auth.module.ts
+++ src/app/pages/admin-auth/admin-auth.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
+import { MaterialModule } from '../../_core/shares/material-module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AdminAuthRoutingModule } from './admin-auth-routing.module';
 import { AdminAuthComponent } from './admin-auth.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AdminAuthComponent
-  ],
+  declarations: [AdminAuthComponent],
   imports: [
     CommonModule,
-    AdminAuthRoutingModule
-  ]
+    AdminAuthRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AdminAuthModule { }
+export class AdminAuthModule {}
