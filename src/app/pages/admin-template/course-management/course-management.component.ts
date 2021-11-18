debaa668eff9631ac82c6680a20971be88642fb4
minh tram <minhtram0901@gmail.com>
2021-11-19 00:10:16 +0700

    done load tabe list course

--- src/app/pages/admin-template/course-management/course-management.component.ts
+++ src/app/pages/admin-template/course-management/course-management.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/_core/services/data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-course-management',
   templateUrl: './course-management.component.html',
-  styleUrls: ['./course-management.component.scss']
+  styleUrls: ['./course-management.component.scss'],
 })
 export class CourseManagementComponent implements OnInit {
-
-  constructor() { }
+  listCourse: any;
+  subListCourse = new Subscription();
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
+    this.getCourse();
+  }
+  getCourse() {
+    this.subListCourse = this.data
+      .get('QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP01')
+      .subscribe((result: any) => {
+        console.log(result);
+        this.listCourse = result;
+      });
   }
 
+  ngOnDestroy() {
+    this.subListCourse.unsubscribe();
+  }
 }
