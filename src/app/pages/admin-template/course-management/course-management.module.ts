a92b90607b33b0c20b98e15afe36c5f7ac269b54
minh tram <minhtram0901@gmail.com>
2021-11-18 22:42:20 +0700

    course management. add materialmodule

--- src/app/pages/admin-template/course-management/course-management.module.ts
+++ src/app/pages/admin-template/course-management/course-management.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from 'src/app/_core/shares/material-module';
 import { CourseManagementRoutingModule } from './course-management-routing.module';
 import { CourseManagementComponent } from './course-management.component';
 
-
 @NgModule({
-  declarations: [
-    CourseManagementComponent
-  ],
-  imports: [
-    CommonModule,
-    CourseManagementRoutingModule
-  ]
+  declarations: [CourseManagementComponent],
+  imports: [CommonModule, CourseManagementRoutingModule, MaterialModule],
 })
-export class CourseManagementModule { }
+export class CourseManagementModule {}
--- src/app/pages/admin-template/user-management/user-management.module.ts
+++ src/app/pages/admin-template/user-management/user-management.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from 'src/app/_core/shares/material-module';
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import { UserManagementComponent } from './user-management.component';
 
-
 @NgModule({
-  declarations: [
-    UserManagementComponent
-  ],
-  imports: [
-    CommonModule,
-    UserManagementRoutingModule
-  ]
+  declarations: [UserManagementComponent],
+  imports: [CommonModule, UserManagementRoutingModule, MaterialModule],
 })
-export class UserManagementModule { }
+export class UserManagementModule {}
