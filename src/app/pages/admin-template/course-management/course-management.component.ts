2b4e0fcf03eb17ea73b2d8d8cbe672f495939bb2
minh tram <minhtram0901@gmail.com>
2021-11-19 15:42:38 +0700

    listCourse done search, pagination

--- src/app/pages/admin-template/course-management/course-management.component.ts
+++ src/app/pages/admin-template/course-management/course-management.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/_core/services/data.service';
 import { Subscription } from 'rxjs';
-import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-course-management',
@@ -12,7 +11,7 @@ export class CourseManagementComponent implements OnInit {
   listCourse: any;
   subListCourse = new Subscription();
   tenKhoaHoc: any;
-  p : number = 1;
+  p: number = 1;
   constructor(private data: DataService) {}
 
   ngOnInit(): void {
@@ -32,22 +31,48 @@ export class CourseManagementComponent implements OnInit {
   }
 
   Search() {
+    this.p = 1;
     if (this.tenKhoaHoc == '') {
       this.ngOnInit();
     } else {
       this.listCourse = this.listCourse.filter((item: any) => {
         console.log(item);
-        return item.tenKhoaHoc
+        return removeVietnameseTones(item.tenKhoaHoc)
           .toLocaleLowerCase()
-          .match(this.tenKhoaHoc.toLocaleLowerCase());
+          .includes(removeVietnameseTones(this.tenKhoaHoc).toLocaleLowerCase());
       });
     }
   }
+}
 
-  key: string = 'id';
-  reserse: boolean = false;
-  sort (key: any){
-    this.key = key;
-    this.reserse = !this.reserse;
-  }
+function removeVietnameseTones(str: string) {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+  str = str.replace(/đ/g, 'd');
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, 'A');
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, 'E');
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, 'I');
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, 'O');
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, 'U');
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, 'Y');
+  str = str.replace(/Đ/g, 'D');
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ''); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ''); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+  // Remove extra spaces
+  // Bỏ các khoảng trắng liền nhau
+  str = str.replace(/ + /g, ' ');
+  str = str.trim();
+  // Remove punctuations
+  // Bỏ dấu câu, kí tự đặc biệt
+  str = str.replace(
+    /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+    ' '
+  );
+  return str;
 }
